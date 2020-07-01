aeb897c32a856859ccbb94fb2161cd2c00e1ba34
Abdullah Zübeyir Yıldız <zubeyir_55@hotmail.com>
2020-07-01 05:51:11 +0300

    Sass

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    {{-- You are logged in! --}}
+                     @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                      {{ __('You are logged in!') }}
+                  <pre>
+                    {{-- {{ dd(json_encode(LaraFlash::allByPriority())) }} --}}
+                  </pre>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
